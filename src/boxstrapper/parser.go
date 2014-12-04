43cc7722fd2369cf080a4174d3c79a66c77a2db5
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-04 02:09:40 +0000

    Add test for single whitespaced group

--- src/boxstrapper/parser.go
+++ src/boxstrapper/parser.go
@@ -13,6 +13,9 @@ func NewPackage(contents string) []Package {
 	parts := strings.SplitN(contents, ": ", 2)
 	pkg := parts[0]
 	groups := strings.Split(parts[1], ",")
+	for i, group := range(groups) {
+		groups[i] = strings.TrimSpace(group)
+	}
 
 	return []Package{Package{Package: pkg, Groups: groups}}
 }
--- src/boxstrapper/parser_test.go
+++ src/boxstrapper/parser_test.go
@@ -16,3 +16,25 @@ func TestSinglePackage(t *testing.T) {
 	assert.Equal(t, 1, len(packages[0].Groups))
 	assert.Equal(t, "system", packages[0].Groups[0])
 }
+
+func TestSinglePackageWithLeadingWhitespace(t *testing.T) {
+	s := "i3:  system"
+
+	packages := NewPackage(s)
+
+	assert.Equal(t, 1, len(packages))
+	assert.Equal(t, "i3", packages[0].Package)
+	assert.Equal(t, 1, len(packages[0].Groups))
+	assert.Equal(t, "system", packages[0].Groups[0])
+}
+
+func TestSinglePackageWithTrailingWhitespace(t *testing.T) {
+	s := "i3: system "
+
+	packages := NewPackage(s)
+
+	assert.Equal(t, 1, len(packages))
+	assert.Equal(t, "i3", packages[0].Package)
+	assert.Equal(t, 1, len(packages[0].Groups))
+	assert.Equal(t, "system", packages[0].Groups[0])
+}
