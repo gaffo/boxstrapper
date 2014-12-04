cd66f7509cc5b0be718052db96256cc48ebe2734
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-04 02:07:20 +0000

    Going with simple parser for packages

--- src/boxstrapper/parser.go
+++ src/boxstrapper/parser.go
@@ -1,10 +1,18 @@
 package boxstrapper
 
+import (
+	"strings"
+)
+
 type Package struct {
 	Package string
 	Groups []string
 }
 
 func NewPackage(contents string) []Package {
-	return []Package{Package{}}
+	parts := strings.SplitN(contents, ": ", 2)
+	pkg := parts[0]
+	groups := strings.Split(parts[1], ",")
+
+	return []Package{Package{Package: pkg, Groups: groups}}
 }
