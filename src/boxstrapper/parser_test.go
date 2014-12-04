6d8af62b8a16572b38b5e41e3809dc2cd83b7aea
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-04 02:11:32 +0000

    SinglePackageMultipleGroups

--- src/boxstrapper/parser_test.go
+++ src/boxstrapper/parser_test.go
@@ -38,3 +38,16 @@ func TestSinglePackageWithTrailingWhitespace(t *testing.T) {
 	assert.Equal(t, 1, len(packages[0].Groups))
 	assert.Equal(t, "system", packages[0].Groups[0])
 }
+
+func TestSinglePackage_MultipleGroups(t *testing.T) {
+	s := "i3: system, boxstrapper"
+
+	packages := NewPackage(s)
+
+	assert.Equal(t, 1, len(packages))
+	assert.Equal(t, "i3", packages[0].Package)
+	assert.Equal(t, 2, len(packages[0].Groups))
+	assert.Equal(t, "system", packages[0].Groups[0])
+	assert.Equal(t, "boxstrapper", packages[0].Groups[1])
+}
+	
