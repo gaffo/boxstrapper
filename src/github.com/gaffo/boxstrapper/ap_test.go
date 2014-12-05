493694c9620c108b71716e83c7c682e7a91fef02
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-05 10:31:47 -0800

    TestApCallsToDriver_MultiplePackages_UnsortedPackages_WithSingleGroup

--- src/github.com/gaffo/boxstrapper/ap_test.go
+++ src/github.com/gaffo/boxstrapper/ap_test.go
@@ -98,21 +98,21 @@ package2: system`).Return(nil).Once()
 	storage.Mock.AssertExpectations(t)
 }
 
-// func TestApCallsToDriver_MultiplePackages_UnsortedPackages_WithSingleGroup(t *testing.T) {
-// 	assert := assert.New(t)
+func TestApCallsToDriver_MultiplePackages_UnsortedPackages_WithSingleGroup(t *testing.T) {
+	assert := assert.New(t)
 
-// 	packages := []string{"package2", "package1"}
-// 	driver := new(mocks.Driver)
-// 	driver.On("AddPackage", "package1").Return(nil).Once()
-// 	driver.On("AddPackage", "package2").Return(nil).Once()
+	packages := []string{"package2:system", "package1:george"}
+	driver := new(mocks.Driver)
+	driver.On("AddPackage", "package1").Return(nil).Once()
+	driver.On("AddPackage", "package2").Return(nil).Once()
 
-// 	storage := new(mocks.Storage)
-// 	storage.On("WritePackages", `package1: default
-// package2: default`).Return(nil).Once()
+	storage := new(mocks.Storage)
+	storage.On("WritePackages", `package1: george
+package2: system`).Return(nil).Once()
 	
-// 	err := Ap(driver, storage, packages)
+	err := Ap(driver, storage, packages)
 
-// 	assert.Nil(err)
-// 	driver.Mock.AssertExpectations(t)
-// 	storage.Mock.AssertExpectations(t)
-// }
+	assert.Nil(err)
+	driver.Mock.AssertExpectations(t)
+	storage.Mock.AssertExpectations(t)
+}
