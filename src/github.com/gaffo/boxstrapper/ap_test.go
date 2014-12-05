90898f38d9a1ff9cd6d194a54b9c50afbab47e40
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-04 20:23:25 -0800

    TestApCallsToDriver_MultiplePackages

--- src/github.com/gaffo/boxstrapper/ap_test.go
+++ src/github.com/gaffo/boxstrapper/ap_test.go
@@ -29,3 +29,17 @@ func TestApCallsToDriver(t *testing.T) {
 	assert.Nil(err)
 	driver.Mock.AssertExpectations(t)
 }
+
+func TestApCallsToDriver_MultiplePackages(t *testing.T) {
+	assert := assert.New(t)
+
+	packages := []string{"package1", "package2"}
+	driver := new(MockDriver)
+	driver.On("AddPackage", "package1").Return(nil).Once()
+	driver.On("AddPackage", "package2").Return(nil).Once()
+	
+	err := Ap(driver, packages)
+
+	assert.Nil(err)
+	driver.Mock.AssertExpectations(t)
+}
