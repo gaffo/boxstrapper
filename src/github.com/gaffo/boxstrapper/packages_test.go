103da60eadba7116a6373acbf03fded8b3b55f60
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-05 10:25:23 -0800

    TestPackageFromApString_SingleGroup, TestPackageFromApString_MultipleGroup

--- src/github.com/gaffo/boxstrapper/packages_test.go
+++ src/github.com/gaffo/boxstrapper/packages_test.go
@@ -254,3 +254,22 @@ func TestPackageFromApString_NoGroup(t *testing.T) {
 	assert.Equal(1, len(pkg.Groups))
 	assert.Equal("default", pkg.Groups[0])
 }
+
+func TestPackageFromApString_SingleGroup(t *testing.T) {
+	assert := assert.New(t)
+
+	pkg := PackageFromApString("package:system")
+	assert.Equal("package", pkg.Package)
+	assert.Equal(1, len(pkg.Groups))
+	assert.Equal("system", pkg.Groups[0])
+}
+
+func TestPackageFromApString_MultipleGroup(t *testing.T) {
+	assert := assert.New(t)
+
+	pkg := PackageFromApString("package:system,george")
+	assert.Equal("package", pkg.Package)
+	assert.Equal(2, len(pkg.Groups))
+	assert.Equal("system", pkg.Groups[0])
+	assert.Equal("george", pkg.Groups[1])
+}
