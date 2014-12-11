1a730f9b9233335a8fcc2636ec4915ce55ad6754
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-11 15:47:29 -0800

    TestReadPackages_RepoWithPackagefile

--- src/github.com/gaffo/boxstrapper/filesystem_storage_test.go
+++ src/github.com/gaffo/boxstrapper/filesystem_storage_test.go
@@ -4,13 +4,30 @@ import (
 	"testing"
 	. "github.com/gaffo/boxstrapper"
   	"github.com/stretchr/testify/assert"
+  	"io/ioutil"
+  	"os"
 )
 
-func TestReadEmptyRepo(t *testing.T) {
+func TestReadPackages_EmptyRepo(t *testing.T) {
 	assert := assert.New(t)
 	storage := NewFilesystemStorage("/nonexistent")
 
 	data, err := storage.ReadPackages()
 	assert.NotNil(err)
 	assert.Equal("", data)
+}
+
+func TestReadPackages_RepoWithPackagefile(t *testing.T) {
+	assert := assert.New(t)
+	_ = os.MkdirAll("tmp/r1", os.ModePerm)
+	defer func () {
+		_ = os.RemoveAll("tmp")
+	}()
+	_ = ioutil.WriteFile("tmp/r1/packages.bss", []byte(`contents`), os.ModePerm)
+
+	storage := NewFilesystemStorage("tmp/r1")
+
+	data, err := storage.ReadPackages()
+	assert.Nil(err)
+	assert.Equal(`contents`, data)
 }
