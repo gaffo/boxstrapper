dbf3261efdfd09b5837fb35b859d41b6cd93c879
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-11 22:54:54 -0800

    TestRecoverMultiplePackages

--- src/github.com/gaffo/boxstrapper/recover_test.go
+++ src/github.com/gaffo/boxstrapper/recover_test.go
@@ -34,3 +34,20 @@ func TestRecoverSinglePackage(t *testing.T) {
 	driver.Mock.AssertExpectations(t)
 	storage.Mock.AssertExpectations(t)
 }
+
+func TestRecoverMultiplePackages(t *testing.T) {
+	assert := assert.New(t)
+	driver := new(mocks.Driver)
+	driver.On("AddPackage", "package").Return(nil).Once()
+	driver.On("AddPackage", "package2").Return(nil).Once()
+
+	storage := new(mocks.Storage)
+	storage.On("ReadPackages").Return(`package: default
+package2: dev`, nil)
+
+	err := Recover(driver, storage)
+	assert.Nil(err)
+
+	driver.Mock.AssertExpectations(t)
+	storage.Mock.AssertExpectations(t)
+}
