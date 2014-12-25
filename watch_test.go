d31455b76b01e8f48ef83f07696a4718639f29be
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-24 22:04:32 -0800

    Test_WatchNoArgs_DoesNothing

--- watch_test.go
+++ watch_test.go
@@ -1,8 +1,18 @@
 package boxstrapper_test
 
 import (
-// . "github.com/gaffo/boxstrapper"
-// "github.com/gaffo/boxstrapper/mocks"
-// "github.com/stretchr/testify/assert"
-// "testing"
+	. "github.com/gaffo/boxstrapper"
+	"github.com/gaffo/boxstrapper/mocks"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
+
+func Test_WatchNoArgs_DoesNothing(t *testing.T) {
+	assert := assert.New(t)
+
+	driver := new(mocks.Driver)
+	storage := new(mocks.Storage)
+
+	err := Watch(driver, storage, []string{})
+	assert.Nil(err)
+}
