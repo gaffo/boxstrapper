88a2faf7861cd3d6ddb3c83b169ea6bdcdd4be6e
Mike Gaffney <mr.gaffo@gmail.com>
2014-12-24 22:02:22 -0800

    Add wrapper for Watch

---
+++ watch.go
@@ -0,0 +1,10 @@
+package boxstrapper
+
+import (
+// "fmt"
+// "strings"
+)
+
+func Watch(driver Driver, storage Storage, watches []string) error {
+	return nil
+}
---
+++ watch_test.go
@@ -0,0 +1,8 @@
+package boxstrapper_test
+
+import (
+// . "github.com/gaffo/boxstrapper"
+// "github.com/gaffo/boxstrapper/mocks"
+// "github.com/stretchr/testify/assert"
+// "testing"
+)
